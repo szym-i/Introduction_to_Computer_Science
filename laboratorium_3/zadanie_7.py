de118a0ad2c03bcb5ef29ef07e7c1fce9dd87670
szym-i <smilczarczyk@student.agh.edu.pl>
2021-10-26 21:38:27 +0200

    zad7kox

--- laboratorium_3/zadanie_7.py
+++ laboratorium_3/zadanie_7.py
@@ -5,19 +5,19 @@ if abs(a-b) > 19:
     print("Zawężam przedział do 6 liczb najbliższym średniej(bez średniej)")
     s = (a+b)/2
     if s%1==0:
-        print(str(s-3)[0:4])
-        print(str(s-2)[0:4])
-        print(str(s-1)[0:4])
-        print(str(s+1)[0:4])
-        print(str(s+2)[0:4])
-        print(str(s+3)[0:4])
+        print(int(s-3))
+        print(int(s-2))
+        print(int(s-1))
+        print(int(s+1))
+        print(int(s+2))
+        print(int(s+3))
     else:
-        print(str(s-2.5)[0:4])
-        print(str(s-1.5)[0:4])
-        print(str(s-0.5)[0:4])
-        print(str(s+0.5)[0:4])
-        print(str(s+1.5)[0:4])
-        print(str(s+2.5)[0:4])
+        print(int(s-2.5))
+        print(int(s-1.5))
+        print(int(s-0.5))
+        print(int(s+0.5))
+        print(int(s+1.5))
+        print(int(s+2.5))
 else:
     if b < a:
         for i in reversed(range (b,a+1)):
