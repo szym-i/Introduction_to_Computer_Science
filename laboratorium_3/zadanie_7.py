b6ae4395a11bb58df3baf22eb7c893e87483c90a
szym-i <smilczarczyk@student.agh.edu.pl>
2021-10-26 17:28:36 +0200

    zad7fix

--- laboratorium_3/zadanie_7.py
+++ laboratorium_3/zadanie_7.py
@@ -1,15 +1,27 @@
-a=int(input(">>>"))
-b=int(input(">>>"))
-if abs(a-b) > 19 :
-    print("Zawężam przedział do 6 liczb")
+print("Wypisz liczby z przedziału")
+a=int(input("OD>"))
+b=int(input("DO>"))
+if abs(a-b) > 19:
+    print("Zawężam przedział do 6 liczb najbliższym średniej(bez średniej)")
     s = (a+b)/2
-    round(s)
-    print(s-3)
-    print(s-2)
-    print(s-1)
-    print(s+1)
-    print(s+2)
-    print(s+3)
+    if s%1==0:
+        print((str(s-3)[0:4]))
+        print((str(s-2)[0:4]))
+        print((str(s-1)[0:4]))
+        print((str(s+1)[0:4]))
+        print((str(s+2)[0:4]))
+        print((str(s+3)[0:4]))
+    else:
+        print((str(s-2.5)[0:4]))
+        print((str(s-1.5)[0:4]))
+        print((str(s-0.5)[0:4]))
+        print((str(s+0.5)[0:4]))
+        print((str(s+1.5)[0:4]))
+        print((str(s+2.5)[0:4]))
 else:
-    for i in range (a,b+1):
-        print(i)
+    if b < a:
+        for i in range (b,a+1):
+            print(i)
+    else:
+        for i in range (a,b+1):
+            print(i)
