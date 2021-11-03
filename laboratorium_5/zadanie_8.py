7823dfc51df417af96c70936b20ed01146e14603
szym-i <smilczarczyk@student.agh.edu.pl>
2021-11-03 14:23:50 +0100

    zadadnie_8

--- laboratorium_5/zadanie_8.py
+++ laboratorium_5/zadanie_8.py
@@ -1,10 +1,1 @@
 #Napisać program wyszukujący liczby doskonałe mniejsze od miliona
-try:
-    n = int(input("Input a positive integer: "))
-except:
-    print("Your input is not a positive integer!")
-    exit()
-
-if (n < 0):
-    print("Your input is not a positive integer!")
-    exit()
