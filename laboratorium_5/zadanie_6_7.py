38287d325db24c151e1e92cf113df0858e7f27e5
szym-i <smilczarczyk@student.agh.edu.pl>
2021-11-03 16:09:09 +0100

    zadadnie_8

--- laboratorium_5/zadanie_6_7.py
+++ laboratorium_5/zadanie_6_7.py
@@ -1,10 +1,10 @@
 a = int(input("Sprawdź czy liczba jest pierwsza: "))  #sprawdź czy liczba jest pierwsza
 b = 0
-for i in range (2,a-1):
+for i in range (1,a+1):
     if a % i == 0:
         b = b+1
         print("Dzielnikiem liczby jest: ",i)
-if b == 0:
+if b <= 2:
     print("Liczba jest pierwsza")
 else:
     print("Liczba nie jest pierwsza")
