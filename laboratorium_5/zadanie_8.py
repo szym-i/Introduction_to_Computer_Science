89fa26c3546206fc8935a5c911ae7681158a32cf
szym-i <smilczarczyk@student.agh.edu.pl>
2021-11-03 16:55:23 +0100

    zadadnie_8

--- laboratorium_5/zadanie_8.py
+++ laboratorium_5/zadanie_8.py
@@ -1,10 +1,10 @@
 print("Wypisz liczby doskonałe < 1000000: ")#Napisać program wyszukujący liczby doskonałe mniejsze od milion
-def suma_dzielnikow(j) -> int:
-    for j in range (1,10000):
+def suma_dzielnikow() -> int:
+    for j in range (1,1000000):
         s = 0
         for i in range (1,j):
             if j % i == 0:
                 s = s + i
         if s == j:
             print('Liczba',j,' jest doskonała!')
-print(suma_dzielnikow(28))
+suma_dzielnikow()
