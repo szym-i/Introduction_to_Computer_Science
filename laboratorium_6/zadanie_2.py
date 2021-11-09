5e34f0dc995ec73dcff9f11833705f23ba49c437
szym-i <smilczarczyk@student.agh.edu.pl>
2021-11-09 14:46:38 +0100

    zespolonev3

--- laboratorium_6/zadanie_2.py
+++ laboratorium_6/zadanie_2.py
@@ -4,7 +4,7 @@ x1 = complex(input("Wprowadź zespoloną>>"))
 x2 = complex(input("Wprowadź drugą liczbę zespoloną>>"))
 print(x1,x2)
 '''zauważyłem że gdy j=0, wynik jest zwracany w mniej czytelnej postaci (X+0j), stąd funkcja sprawdzenie'''
-def sprawdzenie(result): 
+def sprawdzenie(result):
     if result.imag == 0:
         if result.real % 1 == 0:
             return int(result.real)
@@ -27,9 +27,4 @@ def dzielenie(c1,c2):
         return result
     else:
         return '''NIE DZIEL PRZEZ 0'''
-def sprzezenie(c1):
-    if c1.imag == 0:
-        return '''\nBRAK SPRZĘŻENIA,LICZBA NIE JEST ZESPOLONA'''
-    result = c1 -2j*c1.imag
-    return result
-print(f'''Wynik dodawania:{sprawdzenie(suma(x1,x2))}\nWynik odejmowania:{sprawdzenie(roznica(x1,x2))}\nWynik mnożenia:{sprawdzenie(mnozenie(x1,x2))}\nWynik dzielenia:{sprawdzenie(dzielenie(x1,x2))}\nSprzężenia tych liczb są równe:{sprzezenie(x1)},{sprzezenie(x2)}''')
+print(f'''Wynik dodawania:{sprawdzenie(suma(x1,x2))}\nWynik odejmowania:{sprawdzenie(roznica(x1,x2))}\nWynik mnożenia:{sprawdzenie(mnozenie(x1,x2))}\nWynik dzielenia:{sprawdzenie(dzielenie(x1,x2))}\nSprzężenia tych liczb są równe:{sprawdzenie(x1.conjugate())},{sprawdzenie(x2.conjugate())}''')
