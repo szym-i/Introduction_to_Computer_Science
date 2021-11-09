bfb472abaeea9c6d3b5a2a5aea9a7b28af68ae58
szym-i <smilczarczyk@student.agh.edu.pl>
2021-11-09 14:36:30 +0100

    zespolone

--- laboratorium_6/zadanie_2.py
+++ laboratorium_6/zadanie_2.py
@@ -3,31 +3,25 @@ print(f'''UWAGA: liczba zespolona ma postać (A+Bj)''')
 x1 = complex(input("Wprowadź zespoloną>>"))
 x2 = complex(input("Wprowadź drugą liczbę zespoloną>>"))
 print(x1,x2)
+'''zauważyłem że gdy j=0, wynik jest zwracany w mniej czytelnej postaci (X+0j), stąd funkcja sprawdzenie'''
+def sprawdzenie(result): 
+    if result.imag == 0:
+        return result.real
+    else:
+        return result
 def suma(c1,c2):
     result = c1 +c2
-    if result.imag == 0:  #zauważyłem że gdy część urojona = 0, program zwraca wynik w mniej czytelnej formie (X+0j), ten if jest po to aby zwacał sam X gdy imag = 0
-        return result.real
-    else:
-        return result
+    return result
 def roznica(c1,c2):
     result = c1 - c2
-    if result.imag == 0:
-        return result.real
-    else:
-        return result
+    return result
 def mnozenie(c1,c2):
     result = c1 * c2
-    if result.imag == 0:
-        return result.real
-    else:
-        return result
+    return result
 def dzielenie(c1,c2):
     if c2 != 0:
         result = c1/c2
-        if result.imag == 0:
-            return result.real
-        else:
-            return result
+        return result
     else:
         return '''NIE DZIEL PRZEZ 0'''
 def sprzezenie(c1):
@@ -35,4 +29,4 @@ def sprzezenie(c1):
         return '''\nBRAK SPRZĘŻENIA,LICZBA NIE JEST ZESPOLONA'''
     result = c1 -2j*c1.imag
     return result
-print(f'''Wynik dodawania:{suma(x1,x2)}\nWynik odejmowania:{roznica(x1,x2)}\nWynik mnożenia:{mnozenie(x1,x2)}\nWynik dzielenia:{dzielenie(x1,x2)}\nSprzężenia tych liczb są równe:{sprzezenie(x1)},{sprzezenie(x2)}''')
+print(f'''Wynik dodawania:{sprawdzenie(suma(x1,x2))}\nWynik odejmowania:{sprawdzenie(roznica(x1,x2))}\nWynik mnożenia:{sprawdzenie(mnozenie(x1,x2))}\nWynik dzielenia:{sprawdzenie(dzielenie(x1,x2))}\nSprzężenia tych liczb są równe:{sprzezenie(x1)},{sprzezenie(x2)}''')
