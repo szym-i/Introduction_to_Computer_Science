8eeaccb12b09388897916c0ef8fc05be06383d45
szym-i <smilczarczyk@student.agh.edu.pl>
2021-11-11 10:50:03 +0100

    zad2

--- laboratorium_6/zadanie_2.py
+++ laboratorium_6/zadanie_2.py
@@ -1,9 +1,9 @@
-#Proszę napisać program wczytujący dwie liczby urojone w postaci algebraicznej, a następnie wykonać następujące operacje arytmetyczne na tych liczbach
+#Proszę napisać program wczytujący dwie liczby urojone w postaci algebraicznej
 print(f'''UWAGA: liczba zespolona ma postać (A+Bj)''')
 x1 = complex(input("Wprowadź zespoloną>>"))
 x2 = complex(input("Wprowadź drugą liczbę zespoloną>>"))
 print(x1,x2)
-'''zauważyłem że gdy j=0, wynik jest zwracany w mniej czytelnej postaci (X+0j), stąd funkcja sprawdzenie'''
+#zauważyłem że gdy j=0, wynik jest zwracany w nieczytelnej postaci (X+0j), stąd funkcja sprawdzenie
 def sprawdzenie(result):
     if result.imag == 0:
         if result.real % 1 == 0:
@@ -38,4 +38,4 @@ Wynik odejmowania:{sprawdzenie(roznica(x1,x2))}
 Wynik mnożenia:{sprawdzenie(mnozenie(x1,x2))}
 Wynik dzielenia:{sprawdzenie(dzielenie(x1,x2))}
 Sprzężenia tych liczb są równe:{sprawdzenie(x1.conjugate())},{sprawdzenie(x2.conjugate())}
-Potęgi tych liczb są równe:{(potegowanie(x1,x2))}''')
+Potęgi tych liczb są równe:{sprawdzenie(potegowanie(x1,x2))}''')
