0491b87221685f850bdd820fac22eca2472de511
szym-i <smilczarczyk@student.agh.edu.pl>
2021-11-13 00:19:48 +0100

    zad6

--- laboratorium_6/zadanie_6.py
+++ laboratorium_6/zadanie_6.py
@@ -1,4 +1,4 @@
-czynniki = []
+czynniki = [1]
 def rozloz(liczba):
     k = 2
     while liczba != 1:
@@ -6,15 +6,18 @@ def rozloz(liczba):
             liczba //= k
             czynniki.append(k)
         k += 1
-
     return czynniki
 def sprawdz(lista):
     for e in lista:
+        x = 0
         if e != 1 and e != 2 and e != 3 and e != 5:
-            return "Niewłaściwa liczba"
-        else:
-            return "Liczba jest dwu-trzy-piątkowa"
+            x += 1
+    if x == 0:
+        return "Liczba jest dwu-trzy-piątkowa"
+    else:
+        print(f'''Liczba NIE JEST 2,3,5-tkowa''')
 
-n = int(input("Wprowadź swoje N..."))
+n = int(input("Wprowadź swoje N="))
 rozloz(n)
+print(czynniki)
 print(sprawdz(czynniki))
