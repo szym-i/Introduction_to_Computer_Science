d5d7e817f730ecc8191ab66ae2a92fd6e2583418
Szymi <smilczarczyk@student.agh.edu.pl>
2021-11-30 13:55:24 +0100

    zad1v2

--- laboratorium_9/zadanie_1.py
+++ laboratorium_9/zadanie_1.py
@@ -1,10 +1,19 @@
-m = int(input('Podaj ilość wierszy '))
-n = int(input('Podaj ilość kolumn '))
-macierz = []
-for i in range(m):
-    lista = []
-    for j in range(n):
-        x = int(input(f'''Wprowadz {j+1} liczbe w {i+1} wierszu'''))
-        lista.append(x)
-    macierz.append(lista)
-print(macierz)
+def sprawdz(m,n):
+    if m < 1:
+        raise ValueError
+    if n < 1:
+        raise ValueError
+try:
+    m = int(input('Podaj ilość wierszy '))
+    n = int(input('Podaj ilość kolumn '))
+    sprawdz(m,n)
+    macierz = []
+    for i in range(m):
+        lista = []
+        for j in range(n):
+            x = int(input(f'''Wprowadz {j+1} liczbe w {i+1} wierszu '''))
+            lista.append(x)
+        macierz.append(lista)
+    print(macierz)
+except:
+    print('Niewłaściwe wymiary macierzy')
