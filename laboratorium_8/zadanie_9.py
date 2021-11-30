a3e89b8e52e095c2d03813a6407943f2c7ec7e58
Szymi <smilczarczyk@student.agh.edu.pl>
2021-11-30 13:44:43 +0100

    zad9

--- laboratorium_8/zadanie_9.py
+++ laboratorium_8/zadanie_9.py
@@ -1,33 +1,30 @@
-lista_pierwsze = []
+lista_pierwsze = [] #pomocnicza lista
 lista1 = [] #z liczbami półpierwszymi <N, które są iloczynem dwóch różnych liczb pierwszych
 lista2 = [] #z pozostałymi liczbami półpierwszymi <N
-def generuj(n): #przyjmuje liczbe naturalna N
+def generuj(n): #funkcja przyjmuje liczbe naturalna N
     #szukamy liczb pierwszych do polowy liczby N, poniewaz najwiekszy mozliwy iloczyn mniejszy od N
     #jest rowny i_max = 2 * N/2
     for i in range(2,int(n/2)):
-        licznik = 1 #licznik dzielnikow liczby
-        #szukamy dzielnikow do pierwiastka z liczby
-        for j in range(2,int((i**0.5)+1)): #UWAGA: gdy nie dodamy 1, program nie bedzie poprawnie dzialal
+        licznik_dzielnikow = 1
+        for j in range(2,int((i**0.5)+1)):  #szukamy dzielnikow do pierwiastka z liczby +1
             if i % j == 0:
-                licznik += 1 #jesli liczba nie jest pierwsza, licznik sie zwiekszy i program nie wpisze liczby na liste liczb pierwszych
-        if licznik == 1: 
-            lista_pierwsze.append(i) # liczba pierwsza zostanie wpisana na liste
-    for e1 in lista_pierwsze: #bierzemy kazdy element w liscie liczb pierwszych
-        for e2 in lista_pierwsze: #bierzemy kazdy element z tej samej listy
-            if (e2*e1) < n:  #liczba polpierwsza musi byc < N
-                if e2 == e1: #jesli liczba jest iloczynem tych samych liczb wpisujemy na lista2
+                licznik_dzielnikow += 1   #jesli liczba nie jest pierwsza, licznik sie zwiekszy
+        if licznik_dzielnikow == 1: 
+            lista_pierwsze.append(i)
+    for e1 in lista_pierwsze:     #bierzemy kazdy element w liscie liczb pierwszych
+        for e2 in lista_pierwsze:   #znowu bierzemy kazdy element z tej samej listy
+            if (e2*e1) < n:
+                if e2 == e1:
                     lista2.append(e1*e2)
-                else: #w pozostalych przypadkach
-                    if (e1*e2) not in lista1:#wpisujemy na lista1 jesli elementu jeszcze nie ma na lista1
+                else:
+                    if (e1*e2) not in lista1:  #wpisujemy na lista1 jesli elementu jeszcze tam nie ma
                         lista1.append(e1*e2)
     lista1.sort()  # sortujemy liste aby byla czytelniejsza
-    print('Iloczyny dwoch roznych liczb pierwszych(<N):',lista1)
-    print('Iloczyny tych samych liczb pierwszych(<N):',lista2)
-    #print('Liczby pierwsze',lista_pierwsze)
+    return lista1,lista2
 try:
-    n = int(input('Wprowadz swoje naturalne N = '))
-    n = abs(n) #gdyby n bylo calkowite ale ujemne, bierzemy wartosc bezwzgledna
-    generuj(n)
+    N = int(input('Wprowadz swoje naturalne N = '))
+    N = abs(N) #bierzemy wartosc bezwzgledna, gdyby N bylo ujemne
+    print(generuj(N))
 except:
     print('Wprowadzona liczba nie jest naturalna')
 #przypadki testowe --> najwieksza liczba
