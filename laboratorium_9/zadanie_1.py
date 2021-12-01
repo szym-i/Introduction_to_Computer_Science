2aed6ff2fe29cbe5989c1fd5e27d3d495c75cac1
Szymi <smilczarczyk@student.agh.edu.pl>
2021-12-01 16:54:23 +0100

    zad1

--- laboratorium_9/zadanie_1.py
+++ laboratorium_9/zadanie_1.py
@@ -4,15 +4,31 @@ def sprawdz(m,n):
     if n < 1:
         raise ValueError
 try:
-    m = int(input('Podaj ilość wierszy '))
-    n = int(input('Podaj ilość kolumn '))
+    print(f'''Podaj rozmiary macierzy na których będą wykonywane operacje''')
+    m = int(input('Ilość wierszy='))
+    n = int(input('Ilość kolumn='))
     sprawdz(m,n)
-    macierz = []
+    macierz1 = []
+    macierz2 = []
     for i in range(m):
         lista = []
         for j in range(n):
             x = int(input(f'''Wprowadz {j+1} liczbe w {i+1} wierszu '''))
             lista.append(x)
+        macierz1.append(lista)
+    print(macierz1)
+    for i in range(m):
+        lista = []
+        for j in range(n):
+            x = int(input(f'''Wprowadz {j+1} liczbe w {i+1} wierszu '''))
+            lista.append(x)
+        macierz2.append(lista)
+    print(macierz2)
+    macierz = []
+    for k in range(m):
+        lista = []
+        for h in range(n):
+            lista.append(macierz1[k][h] + macierz2[k][h])
         macierz.append(lista)
     print(macierz)
 except:
