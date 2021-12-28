1668b48c1a11f436a5ecd15ad6e48ff206472a94
Szymi <smilczarczyk@student.agh.edu.pl>
2021-12-28 18:52:09 +0100

    lab 12 chad

--- laboratorium_12/zadanie_6.py
+++ laboratorium_12/zadanie_6.py
@@ -1,43 +1,36 @@
 #from typing import AsyncContextManager
-
-
 class NotEnoughPointsError(Exception):
     pass
 właściwe_prostokąty = []
-def CheckIfDuplicate(posortowane):
-    if posortowane not in właściwe_prostokąty:
-        właściwe_prostokąty.append(posortowane)
+def CheckIfDuplicate(prostokąt):
+    posortowany = sorted(prostokąt, key=lambda tup: tup) #sortujemy po krotkach
+    if posortowany not in właściwe_prostokąty:
+        właściwe_prostokąty.append(posortowany)
 def EnoughPointsCheck(dane):
     if len(dane) < 4:
         raise NotEnoughPointsError
 def Look4Rectangle(dane):
     for point1 in dane:
-        #print('Sprawdzam teraz---->',point1)
         for point2 in dane:
-            #print('Sprawdzam w pionie...')
             if point1[0] == point2[0] and point1 != point2:
-                #print(point2)
-                #print("Sprawdzam w pioziomie...")
                 for point3 in dane:
                     if point1[1] == point3[1] and point1 != point3:
-                        #print(point3)
                         for point4 in dane:
                             if point2[1] == point4[1] and point4[0] == point3[0] and point2 != point4:
-                                #print(point4)
-                                #print('Znalazłem prostokąt o punktach:',point1,point2,point3,point4)
-                                prostokąty = [point1,point2,point3,point4]
+                                prostokąt = [point1,point2,point3,point4]
                                 flag = True
                                 for point5 in dane:
-                                    if point5[1] > min(point1[1],point2[1]) and point5[1] < max(point1[1],point2[1]): #rozbiłem to na dwa ify dla lepszej czytelności
-                                        if point5[0] > min(point1[0],point3[0]) and point5[0] < max(point1[0],point3[0]):
-                                            flag = False
+                                    min_y = min(point1[1],point2[1])
+                                    max_y = max(point1[1],point2[1])
+                                    min_x = min(point1[0],point3[0])
+                                    max_x = max(point1[0],point3[0])
+                                    if point5[1] > min_y and point5[1] < max_y and point5[0] > min_x and point5[0] < max_x: 
+                                        flag = False
                                     length1 = abs(point1[1] - point2[1])
                                     length2 = abs(point1[0] - point3[0])
                                 if length1 != length2 and flag == True: #sprawdzamy czy prostokąt nie jest kwadratem
-                                    posortowane = sorted(prostokąty, key=lambda tup: tup) #sortujemy po krotkach
-                                    CheckIfDuplicate(posortowane)
-dane = [(1,4),(6,4),(6,1),(1,1),(0,2),(1,5),(8,6),(0,0),(0,1),(1,0),(0,4),(6,0)]
-
+                                    CheckIfDuplicate(prostokąt)
+dane = [(1,4),(6,4),(6,1),(1,1),(0,2),(1,5),(8,6),(0,0)]
 try:
     EnoughPointsCheck(dane)
     Look4Rectangle(dane)
