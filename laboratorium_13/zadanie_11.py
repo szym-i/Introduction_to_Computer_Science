97bffefd061efa75953bf60ed0f647b413948940
Szymi <smilczarczyk@student.agh.edu.pl>
2022-01-04 14:51:10 +0100

    lab13_ez

--- laboratorium_13/zadanie_11.py
+++ laboratorium_13/zadanie_11.py
@@ -1,16 +1,16 @@
 def Max(lista,maks,n): #funkcja do znalezienia max elementu
     if n > 0:
         n -= 1 #dekrementujemy n 
-        if lista[n-1] > maks:
+        if lista[n-1] > maks: #n-1 bo elementy w liście są indeksowane od 0
             maks = lista[n-1]
-        Max(lista,maks,n)
+        Max(lista,maks,n) #rekurencja
     elif n == 0:
         print('Maksymalny element w liście to',maks)
 def Suma(lista,suma,n): #funkcja do obliczenia sumy
     if n > 0:
         n -= 1 #dekrementujemy n
-        suma += lista[n-1] #wyciągamy ostatni element z listy
-        Suma(lista,suma,n)
+        suma += lista[n-1] #dodajemy element n-1 z listy
+        Suma(lista,suma,n) #rekurencja
     elif n == 0:
         print('Suma elementów listy to',suma)
 lista = [1,2,3,4]
